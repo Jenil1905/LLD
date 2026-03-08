33bddada0d2a2fc30c98d0ebb637ec863593af62
Jenil1905 <langaliyajenil@gmail.com>
2026-03-09 01:28:26 +0530

    Done proxy ex

--- proxy-reports/src/com/example/reports/ReportViewer.java
+++ proxy-reports/src/com/example/reports/ReportViewer.java
@@ -7,7 +7,10 @@ package com.example.reports;
  */
 public class ReportViewer {
 
-    public void open(ReportFile report, User user) {
+    // public void open(ReportFile report, User user) {
+    //     report.display(user);
+    // }
+    public void open(Report report, User user) {
         report.display(user);
     }
 }
